32320eedda73ce18aec6204d7f741f206442a63a
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-17 19:34:35 +0330

    Delete fragment_and_tiktok.sh

--- fragment_and_tiktok.sh
+++
@@ -1,9 +0,0 @@
-wget "https://raw.githubusercontent.com/mashkouk/hidy/refs/heads/main/base_xray_config.json.j2"
-sudo cp base_xray_config.json.j2 /opt/hiddify-manager/.venv/lib/python3.10/site-packages/hiddifypanel/panel/user/templates/
-cd /root
-rm -rf base_xray_config.json.j2
-wget "https://github.com/mashkouk/hidy/raw/refs/heads/main/user.py"
-sudo cp user.py /opt/hiddify-manager/.venv/lib/python3.10/site-packages/hiddifypanel/models/
-cd /root
-rm -rf user.py
-bash /opt/hiddify-manager/apply_configs.sh
