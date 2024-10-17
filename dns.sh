03d0386569962dfe38d3231625bb7688043c3ef3
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-17 18:36:12 +0330

    Update dns.sh

--- dns.sh
+++ dns.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-# نصب پکیج resolvconf
 sudo apt install -y resolvconf
-
-# ویرایش فایل resolv.conf.d/head
-sudo tee -a /etc/resolvconf/resolv.conf.d/head << EOF
-nameserver 8.8.4.4
-nameserver 9.9.9.9
-nameserver 1.1.1.1
-#
-EOF
-
-# ریبوت کردن سیستم
-# sudo reboot
+wget https://github.com/mashkouk/hidy/raw/refs/heads/main/head
+sudo cp head /etc/resolvconf/resolv.conf.d/
+sudo reboot
