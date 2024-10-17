d17946f8f9ed7995a1297ad614f80e4a9350d908
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-17 17:12:36 +0330

    Update fragment_and_tiktok.sh

--- fragment_and_tiktok.sh
+++ fragment_and_tiktok.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-
 # آدرس فایل در GitHub
 source_url="https://raw.githubusercontent.com/mashkouk/hidy/refs/heads/main/base_xray_config.json.j2"
-
 # مسیر مقصد در سیستم
 destination_dir="/opt/hiddify-manager/.venv/lib/python3.10/site-packages/hiddifypanel/panel/user/templates/"
-
 # نام فایل مقصد
 destination_file="base_xray_config.json.j2"
-
 # ایجاد مسیر مقصد در صورت وجود نداشتن
 # mkdir -p "$destination_dir"
-
 # دانلود فایل از GitHub و قرار دادن آن در مسیر مقصد
 wget -O "$destination_dir/$destination_file" "$source_url"
-
 # بررسی نتیجه دانلود
 if [ $? -eq 0 ]; then
   echo "فایل با موفقیت دانلود و جایگزین شد."
