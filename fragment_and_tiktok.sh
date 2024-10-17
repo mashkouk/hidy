4623bf8f4ee5d3df773a0491d927b0ee567fc91d
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-17 13:09:56 +0330

    Add files via upload
    set fragment and fix tiktok

---
+++ fragment_and_tiktok.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# آدرس فایل در GitHub
+source_url="https://raw.githubusercontent.com/mashkouk/hidy/refs/heads/main/base_xray_config.json.j2"
+
+# مسیر مقصد در سیستم
+destination_dir="/opt/hiddify-manager/.venv/lib/python3.10/site-packages/hiddifypanel/panel/user/templates/"
+
+# نام فایل مقصد
+destination_file="base_xray_config.json.j2"
+
+# ایجاد مسیر مقصد در صورت وجود نداشتن
+# mkdir -p "$destination_dir"
+
+# دانلود فایل از GitHub و قرار دادن آن در مسیر مقصد
+wget -O "$destination_dir/$destination_file" "$source_url"
+
+# بررسی نتیجه دانلود
+if [ $? -eq 0 ]; then
+  echo "فایل با موفقیت دانلود و جایگزین شد."
+else
+  echo "خطا در دانلود فایل."
+fi
