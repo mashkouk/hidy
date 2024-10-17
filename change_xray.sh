6ade1e8980d677cfe0903a8fd610271dedefdce3
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-17 21:53:21 +0330

    Delete change_xray.sh

--- change_xray.sh
+++
@@ -1,8 +0,0 @@
-service hiddify-xray stop
-service hiddify-haproxy stop
-rm -rf /opt/hiddify-manager/xray/bin/*
-wget https://github.com/GFW-knocker/Xray-core/releases/download/v1.8.23-mahsa-r3/Xray-freebsd-arm64-v8a.zip -O xray.zip
-unzip xray.zip -d /opt/hiddify-manager/xray/bin/
-rm xray.zip
-chmod +x /opt/hiddify-manager/xray/bin/xray
-bash /opt/hiddify-manager/restart.sh
