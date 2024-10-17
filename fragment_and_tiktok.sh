06e87a021b60d57ba76723e47accdb1769914900
mashkoukras ( reza as ) <55289441+mashkouk@users.noreply.github.com>
2024-10-17 20:19:00 +0330

    Update fragment_and_tiktok.sh

--- fragment_and_tiktok.sh
+++ fragment_and_tiktok.sh
@@ -1,4 +1,3 @@
-IFS=$' \t\n'
 wget https://raw.githubusercontent.com/mashkouk/hidy/refs/heads/main/base_xray_config.json.j2
 sudo cp base_xray_config.json.j2 /opt/hiddify-manager/.venv/lib/python3.10/site-packages/hiddifypanel/panel/user/templates/
 cd /root
